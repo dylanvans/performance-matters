2657fd5d0803dc49f0c4df78f9936ae2a0bb6e76
dylanvans <dylanvansprang@gmail.com>
2017-03-17 10:26:19 +0100

    gzip compression and AUDIT.md

---
+++ AUDIT.md
@@ -0,0 +1,12 @@
+# Performance audit
+## Gzip compression
+### Timeline before minimizing files - 23s
+![Screenshot of first timeline](https://github.com/dylanvans/performance-matters/blob/image-optimization/md-img/first-screenshot.png)
+
+### Timeline after minimizing files - 21s
+![Screenshot of timeline](https://github.com/dylanvans/performance-matters/blob/gzip-compression/md-img/screenshot.png)
+
+#### Sources
+- [Npm compression](https://www.npmjs.com/package/compression)
+
+
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const compression = require('compression');
 const routeStatic = require('./lib/route-static');
 const redirectIndices = require('./lib/redirect-indices');
 
 const app = express();
 const baseDir = 'src';
 const port = process.env.PORT || 3004;
+
+app.use(compression());
 
 app.set('etag', false);
 app.use((req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
