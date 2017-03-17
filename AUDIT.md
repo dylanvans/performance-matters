d50d8e2a44b6e3479870f12d2179341cc5374597
dylanvans <dylanvansprang@gmail.com>
2017-03-17 10:12:06 +0100

    AUDIT.md and gulp minimizing

---
+++ AUDIT.md
@@ -0,0 +1,16 @@
+# Performance audit
+## File minimization
+### Timeline before minimizing files - 23s
+![Screenshot of first timeline](https://github.com/dylanvans/performance-matters/blob/image-optimization/md-img/first-screenshot.png)
+
+### Timeline after minimizing files - 21s
+![Screenshot of timeline](https://github.com/dylanvans/performance-matters/blob/image-optimization/md-img/screenshot.png)
+
+#### Sources
+- [Gulp uglify](https://www.npmjs.com/package/gulp-uglify)
+- [Gulp Concat](https://www.npmjs.com/package/gulp-concat)
+- [Gulp clean css](https://www.npmjs.com/package/gulp-clean-css)
+- [Gulp concat css](https://www.npmjs.com/package/gulp-concat-css)
+- [Gulp htmlmin](https://www.npmjs.com/package/gulp-htmlmin)
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,40 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+cleanCSS = require('gulp-clean-css'),
+concatCss = require('gulp-concat-css')
+htmlmin = require('gulp-htmlmin');
+
+gulp.task('htmlmin', function() {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('src/'));
+});
+
+gulp.task('cssmin', function() {
+  return gulp.src('src/dist/css/' + '*.css')
+  	.pipe(concatCss('bundle.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('src/dist/css/min'));
+});
+
+gulp.task('cssmin-assets', function() {
+  return gulp.src('src/assets/css/src/docs.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('src/assets/css/src/'));
+});
+
+gulp.task('jsmin', function() {
+	return gulp.src('src/dist/js/bootstrap.js')
+	.pipe(concat('js.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/dist/js/'));
+});
+
+gulp.task('jsmin-assets', function() {
+	return gulp.src('src/assets/js/ie10-viewport-bug-workaround.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('src/assets/js/'));
+});
 
 gulp.task('default', ['']);
